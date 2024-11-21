3e8327f95977e43f1d5fca31a45d7ffa4ad76e0b
icebrick <icebrick0312@gmail.com>
2024-11-21 18:05:31 +0800

    Minor bug fix

--- App/Pages/Error.ts
+++ App/Pages/Error.ts
@@ -6,7 +6,7 @@ export default (args: { error: string }) => {
     description: 'Something went wrong.',
     content: new Element('body', { style: { display: 'flex', flexDirection: 'column', center: 'horizontal vertical', backgroundColor: '$background_color', margin: '0px', width: '100dvw', height: '100dvh' } }, [
       new Element('h1', { class: 'text', style: { fontSize: '4rem' }, innerHTML: 'Error' }),
-      new Element('h1', { class: 'text', style: { fontSize: '1.25rem' }, innerHTML: `${args.error}` })
+      new Element('h1', { class: 'text', style: { fontSize: '1.25rem', maxWidth: '90dvw' }, innerHTML: `${args.error}` })
     ])
   }
 }
